20a528c4ecf08fbe9e9f9830d91f23068ae94a10
Amitk4600 <122792137+Amitk4600@users.noreply.github.com>
2023-06-25 16:02:19 +0530

    Update q2.js

--- q2.js
+++ q2.js
@@ -4,7 +4,7 @@ First Unique Character in a String
 Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
 Example 1:
-Input: s = "leetcode"
+Input: s = "loveleetcode"
 Output: 0
 
 */
